a5333825721be7f89e52f53d01f63248cdfe93b4
Josiah Ross <josiahnross@gmail.com>
2022-11-16 17:23:14 -0800

    Modified Footer to Link to Github Link from System Info Source Repo

--- frontend/src/fixtures/systemInfoFixtures.js
+++ frontend/src/fixtures/systemInfoFixtures.js
@@ -2,12 +2,14 @@ const systemInfoFixtures = {
     showingBoth:
     {
         "springH2ConsoleEnabled": true,
-        "showSwaggerUILink": true
+        "showSwaggerUILink": true,
+        "sourceRepo": "https://github.com/ucsb-cs156-f22/f22-5pm-happycows"
     },
     showingNeither:
     {
         "springH2ConsoleEnabled": false,
-        "showSwaggerUILink": false
+        "showSwaggerUILink": false,
+        "sourceRepo": "https://github.com/ucsb-cs156-f22/f22-5pm-happycows"
     }
 };
 
