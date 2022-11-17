5aea34a189a06a282c95b20f2616dc31600b934d
Cyril Wang <cyrilwang@umail.ucsb.edu>
2022-11-17 00:40:24 -0800

    cw - fixed mapping, switched from GetMapping to POSTMapping for jobMilkCows

--- src/main/java/edu/ucsb/cs156/happiercows/controllers/JobsController.java
+++ src/main/java/edu/ucsb/cs156/happiercows/controllers/JobsController.java
@@ -66,7 +66,7 @@ public class JobsController extends ApiController {
 
     @ApiOperation(value = "Launch Job to Milk the Cows")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
-    @GetMapping("/launch/testjob")
+    @POSTMapping("/launch/testjob")
     public Job jobMilkCows() 
     {
         return jobService.runAsJob(ctx -> {
