7d824352a61b8a8a70f85e27f335af019882dede
Phillip Conrad <pconrad@cs.ucsb.edu>
2022-11-20 10:48:44 -0800

    pc - add Beans needed to ensure that Async threads operate in same security context as thread that spawned them

--- src/main/java/edu/ucsb/cs156/happiercows/HappierCowsApplication.java
+++ src/main/java/edu/ucsb/cs156/happiercows/HappierCowsApplication.java
@@ -6,6 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.security.task.DelegatingSecurityContextAsyncTaskExecutor;
+
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableAsync
@@ -13,6 +17,24 @@ public class HappierCowsApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(HappierCowsApplication.class, args);
+  }
+
+  // See: https://www.baeldung.com/spring-security-async-principal-propagation
+  @Bean
+  public DelegatingSecurityContextAsyncTaskExecutor taskExecutor(ThreadPoolTaskExecutor delegate) {
+    return new DelegatingSecurityContextAsyncTaskExecutor(delegate);
+  }
+
+  // See: https://www.baeldung.com/spring-security-async-principal-propagation
+  @Bean
+  public ThreadPoolTaskExecutor threadPoolTaskExecutor() {
+    ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+    executor.setCorePoolSize(2);
+    executor.setMaxPoolSize(2);
+    executor.setQueueCapacity(500);
+    executor.setThreadNamePrefix("HappierCows-");
+    executor.initialize();
+    return executor;
   }
 
 }
