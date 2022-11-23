305269510939735966cd039656dc16d20e396c65
Alexander Robles <alexanderrobles@umail.ucsb.edu>
2022-11-22 18:13:13 -0800

    ar - added instructorReport and updateCowHealth job placeholders

---
+++ frontend/src/main/components/Jobs/UpdateCowHealthForm.js
@@ -0,0 +1,8 @@
+function UpdateCowHealthForm() {
+    return (
+      <p>This will be the UpdateCowHealthForm Job Function! (Creating this form asap Money!)</p>
+    );
+  }
+  
+  export default UpdateCowHealthForm;
+  
