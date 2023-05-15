8e40f5b784b820b2a57dfb2989f5474faebe9905
Phill Conrad <phtcon@ucsb.edu>
2023-05-15 15:22:46 -0700

    pc - update javadoc and remove commented out code

--- src/main/java/edu/ucsb/cs156/happiercows/jobs/ScheduledJobs.java
+++ src/main/java/edu/ucsb/cs156/happiercows/jobs/ScheduledJobs.java
@@ -6,6 +6,19 @@ import org.springframework.stereotype.Component;
 import edu.ucsb.cs156.happiercows.services.jobs.JobService;
 import lombok.extern.slf4j.Slf4j;
 
+/**
+ * This class contains methods that are scheduled to run at certain times
+ * to launch particular jobs.
+ * 
+ * The value of the <code>cron</code> parameter to the <code>&#64;Scheduled</code>
+ * annotation is a Spring Boot cron expression, which is similar to
+ * a Unix cron expression, but with an extra field at the beginning for
+ * the seconds.
+ * 
+ * @See <a href="https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/scheduling/support/CronExpression.html">Spring Cron Syntax</a>
+ * 
+ */
+
 @Component("scheduledJobs")
 @Slf4j
 public class ScheduledJobs {
@@ -28,15 +41,6 @@ public class ScheduledJobs {
     
        log.info("runUpdateCowHealthJobBasedOnCron: launched job");
     }
-
-    // /**
-    //  * A job that runs every ten seconds, as an example
-    //  */
-
-    // @Scheduled(cron = "*/10 * * * * * ")
-    // public void everyTenSeconds() {
-    //    log.info("everyTenSeconds: running");
-    // }
 
     @Scheduled(cron = "${app.milkTheCows.cron}")
     public void runMilkTheCowsJobBasedOnCron() {
