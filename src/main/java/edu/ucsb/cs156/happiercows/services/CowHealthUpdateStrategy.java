8fdada203e7ed75294375f4756a517c04d14202c
GlassBricks <24237065+GlassBricks@users.noreply.github.com>
2023-05-24 14:13:53 -0700

    by - add CowHealthUpdateStrategy as enum

---
+++ src/main/java/edu/ucsb/cs156/happiercows/services/CowHealthUpdateStrategy.java
@@ -0,0 +1,56 @@
+package edu.ucsb.cs156.happiercows.services;
+
+import edu.ucsb.cs156.happiercows.entities.Commons;
+import edu.ucsb.cs156.happiercows.entities.UserCommons;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.Optional;
+
+@Getter
+@AllArgsConstructor
+public enum CowHealthUpdateStrategy {
+
+    Linear("Linear", "Cow health increases/decreases proportionally to the number of cows over/under the carrying capacity.") {
+        @Override
+        public double calculateNewCowHealth(Commons commons, UserCommons user, int totalCows) {
+            return user.getCowHealth() - (totalCows - commons.getCarryingCapacity()) * commons.getDegradationRate();
+        }
+    },
+    Constant("Constant", "Cow health changes increases/decreases by the degradation rate, depending on if the number of cows exceeds the carrying capacity.") {
+        @Override
+        public double calculateNewCowHealth(Commons commons, UserCommons user, int totalCows) {
+            if (totalCows <= commons.getCarryingCapacity()) {
+                return user.getCowHealth() + commons.getDegradationRate();
+            } else {
+                return user.getCowHealth() - commons.getDegradationRate();
+            }
+        }
+    },
+    Noop("Do nothing", "Cow health does not change.") {
+        @Override
+        public double calculateNewCowHealth(Commons commons, UserCommons user, int totalCows) {
+            return user.getCowHealth();
+        }
+    };
+
+
+    private final String displayName;
+    private final String description;
+
+    public abstract double calculateNewCowHealth(
+            Commons commons,
+            UserCommons user,
+            int totalCows
+    );
+
+    public static Optional<CowHealthUpdateStrategy> getByName(String name) {
+        try {
+            return Optional.of(CowHealthUpdateStrategy.valueOf(name));
+        } catch (IllegalArgumentException e) {
+            return Optional.empty();
+        }
+
+    }
+
+}
