0591a2c5e1b64d96e1c4bfb657f804f128157d2f
albertyu03 <albertyu@ucsb.edu>
2023-05-29 23:17:59 -0700

    removing commented out/deprecated systemInfo/footer console logs

--- frontend/src/tests/components/Nav/Footer.test.js
+++ frontend/src/tests/components/Nav/Footer.test.js
@@ -14,7 +14,6 @@ describe("Footer tests", () => {
         expect(text.textContent).toEqual('HappierCows is a project of Mattanjah de Vries, Distinguished Professor of Chemistry at UC Santa Barbara. The open source code is available on GitHub.');
     
         const href = screen.getByTestId("github-href");
-        //console.log(href.href);
         expect(href).toHaveAttribute("href", "https://github.com/ucsb-cs156/proj-happycows");
     });
 
@@ -25,12 +24,8 @@ describe("Footer tests", () => {
 
         const text = screen.getByTestId("footer-content");
         expect(text).toBeInTheDocument();
-        // expect(typeof(text.textContent)).toBe('string');
-        // expect(text.textContent).toEqual('HappierCows is a project of Mattanjah de Vries, Distinguished Professor of Chemistry at UC Santa Barbara. The open source code is available on GitHub.');
-    
+            
         const href = screen.getByTestId("github-href");
-        //console.log(href.href);
         expect(href.href).toBe("");
-        // expect(href).toHaveAttribute("href", undefined);
     });
 });
