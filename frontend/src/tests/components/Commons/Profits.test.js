405b257a0045a9322013ae6fa99a17c910c00763
Max Fletcher <mfletcher@umail.ucsb.edu>
2023-06-02 20:16:06 -0700

    mf - Profits tests to use new names

--- frontend/src/tests/components/Commons/Profits.test.js
+++ frontend/src/tests/components/Commons/Profits.test.js
@@ -15,7 +15,7 @@ describe("Profits tests", () => {
             <Profits userCommons={userCommonsFixtures.oneUserCommons[0]}  />
         );
         await waitFor(()=>{
-            expect(screen.getByTestId("ProfitsTable-header-Amount") ).toBeInTheDocument();
+            expect(screen.getByTestId("ProfitsTable-header-Profit") ).toBeInTheDocument();
         });
     });
 
@@ -24,18 +24,18 @@ describe("Profits tests", () => {
             <Profits userCommons={userCommonsFixtures.oneUserCommons[0]} profits={profitsFixtures.threeProfits} />
         );
            
-        expect(await screen.findByTestId("ProfitsTable-cell-row-0-col-Amount")).toBeInTheDocument();
-        expect(screen.getByTestId("ProfitsTable-cell-row-0-col-Amount")).toHaveTextContent(/58.20/);
-        expect(screen.getByTestId("ProfitsTable-cell-row-1-col-Amount")).toHaveTextContent(/54.60/);
-        expect(screen.getByTestId("ProfitsTable-cell-row-2-col-Amount")).toHaveTextContent(/52.80/);
+        expect(await screen.findByTestId("ProfitsTable-cell-row-0-col-Profit")).toBeInTheDocument();
+        expect(screen.getByTestId("ProfitsTable-cell-row-0-col-Profit")).toHaveTextContent(/58.20/);
+        expect(screen.getByTestId("ProfitsTable-cell-row-1-col-Profit")).toHaveTextContent(/54.60/);
+        expect(screen.getByTestId("ProfitsTable-cell-row-2-col-Profit")).toHaveTextContent(/52.80/);
 
         expect(screen.getByTestId("ProfitsTable-cell-row-0-col-date")).toHaveTextContent(/2023-05-15/);
         expect(screen.getByTestId("ProfitsTable-cell-row-1-col-date")).toHaveTextContent(/2023-05-16/);
         expect(screen.getByTestId("ProfitsTable-cell-row-2-col-date")).toHaveTextContent(/2023-05-17/);
 
-        expect(screen.getByTestId("ProfitsTable-cell-row-0-col-avgCowHealth")).toHaveTextContent(/97/);
-        expect(screen.getByTestId("ProfitsTable-cell-row-1-col-avgCowHealth")).toHaveTextContent(/91/);
-        expect(screen.getByTestId("ProfitsTable-cell-row-2-col-avgCowHealth")).toHaveTextContent(/88/);
+        expect(screen.getByTestId("ProfitsTable-cell-row-0-col-Health")).toHaveTextContent(/97%/);
+        expect(screen.getByTestId("ProfitsTable-cell-row-1-col-Health")).toHaveTextContent(/91%/);
+        expect(screen.getByTestId("ProfitsTable-cell-row-2-col-Health")).toHaveTextContent(/88%/);
 
         expect(screen.getByTestId("ProfitsTable-cell-row-0-col-numCows")).toHaveTextContent(/6/);
         expect(screen.getByTestId("ProfitsTable-cell-row-1-col-numCows")).toHaveTextContent(/6/);
