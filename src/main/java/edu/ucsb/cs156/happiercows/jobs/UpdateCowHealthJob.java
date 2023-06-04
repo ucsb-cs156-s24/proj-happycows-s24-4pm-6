4dcd7298262f90fca6ee55b74c34556d85483adf
JoeIsNoJoe <joezhang@ucsb.edu>
2023-06-03 18:59:21 -0700

    fixed logic error with updating correct health after calculateCowDeaths run

--- src/main/java/edu/ucsb/cs156/happiercows/jobs/UpdateCowHealthJob.java
+++ src/main/java/edu/ucsb/cs156/happiercows/jobs/UpdateCowHealthJob.java
@@ -42,14 +42,14 @@ public class UpdateCowHealthJob implements JobContextConsumer {
             for (UserCommons userCommons : allUserCommons) {
                 User user = userRepository.findById(userCommons.getUserId()).orElseThrow(() -> new RuntimeException("Error calling userRepository.findById(" + userCommons.getUserId() + ")"));
                 ctx.log("User: " + user.getFullName() + ", numCows: " + userCommons.getNumOfCows() + ", cowHealth: " + userCommons.getCowHealth());
+                var newCowHealth = calculateNewCowHealthUsingStrategy(cowHealthUpdateStrategy, commons, userCommons, totalCows);
                 
                 int temp = userCommons.getCowDeaths();
                 calculateCowDeaths(userCommons, ctx);
                 if (userCommons.getCowDeaths() > temp) {
-                    totalCows = 0;
+                    newCowHealth = 100.0;
                 }
                 
-                var newCowHealth = calculateNewCowHealthUsingStrategy(cowHealthUpdateStrategy, commons, userCommons, totalCows);
                 ctx.log(" old cow health: " + userCommons.getCowHealth() + ", new cow health: " + newCowHealth);
                 userCommons.setCowHealth(newCowHealth);
                 userCommonsRepository.save(userCommons);
