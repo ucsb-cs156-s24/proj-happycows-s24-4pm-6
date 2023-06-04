fc9f17c7043c14483fef69da7b9ad69c071525ec
Max Fletcher <mfletcher@umail.ucsb.edu>
2023-06-04 14:21:33 -0700

    mf - Add test for daysSinceTimestamp

--- frontend/src/main/utils/dateUtils.js
+++ frontend/src/main/utils/dateUtils.js
@@ -6,15 +6,11 @@ const timestampToDate = (timestamp) => {
     return (date.getFullYear() + "-" + (padWithZero(date.getMonth()+1)) + "-" + padWithZero(date.getDate()));
 }
 
-// Disabling stryker here because the returned value depends on the date the test is run
-// so a deterministic test is not possible
-// Stryker disable all
 const daysSinceTimestamp = (date) => {
     var today = new Date();
     var startingDate = new Date(date);
     var timeDiff = Math.abs(today.getTime() - startingDate.getTime());
     return Math.ceil(timeDiff / (1000 * 3600 * 24));
 }
-// Stryker enable all
 
 export {timestampToDate, padWithZero, daysSinceTimestamp};
