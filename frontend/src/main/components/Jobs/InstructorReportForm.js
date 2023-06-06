bc04a20695d026f772273d0dee86877abe3ac94c
albertyu03 <albertyu@ucsb.edu>
2023-06-06 11:35:04 -0700

    adding button for instructorreport

--- frontend/src/main/components/Jobs/InstructorReportForm.js
+++ frontend/src/main/components/Jobs/InstructorReportForm.js
@@ -1,8 +1,16 @@
+import { Button, Form } from "react-bootstrap";
+import { useForm } from "react-hook-form";
+
 function InstructorReportForm() {
-    return (
-      <p>This will be the Instructor Report Function (coming soon)</p>
-    );
-  }
-  
-  export default InstructorReportForm;
-  
+  const {
+    handleSubmit,
+  } = useForm(
+  );
+
+  return (
+    <Form onSubmit={handleSubmit(submitAction)}>
+      <p>Click this button to generate an instructor report!</p>
+      <Button type="submit" data-testid="InstructorReport-Submit-Button">Submit</Button>
+  </Form>
+  );
+}
