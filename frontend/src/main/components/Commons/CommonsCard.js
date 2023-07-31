b6e4cdff3f5be924aecdcd679c35952516aba2fb
Phillip Conrad <pconrad@cs.ucsb.edu>
2023-07-31 16:11:41 -0700

    pc - 100% mutation coverage for HomePage.js

--- frontend/src/main/components/Commons/CommonsCard.js
+++ frontend/src/main/components/Commons/CommonsCard.js
@@ -10,8 +10,8 @@ const CommonsCard = ({ buttonText, buttonLink, commons }) => {
         }>
             <Container>
                 <Row>
-                    <Col sx={4} data-testid={`${testIdPrefix}-id`}>{commons.id}</Col>
-                    <Col sx={4} data-testid={`${testIdPrefix}-name`}>{commons.name}</Col>
+                    <Col sx={4} data-testid={`${testIdPrefix}-id-${commons.id}`}>{commons.id}</Col>
+                    <Col sx={4} data-testid={`${testIdPrefix}-name-${commons.id}`}>{commons.name}</Col>
                     {buttonText != null &&
                         <Col sm={4}>
                             <Button
--- frontend/src/tests/pages/HomePage.test.js
+++ frontend/src/tests/pages/HomePage.test.js
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { MemoryRouter } from "react-router-dom";
 import axios from "axios";
@@ -9,11 +9,21 @@ import commonsFixtures from "fixtures/commonsFixtures";
 import { apiCurrentUserFixtures } from "fixtures/currentUserFixtures";
 import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
 
+const mockNavigate = jest.fn();
+jest.mock("react-router-dom", () => ({
+    ...jest.requireActual("react-router-dom"),
+    useParams: () => ({
+        commonsId: 1
+    }),
+    useNavigate: () => mockNavigate
+}));
+
 describe("HomePage tests", () => {
     const queryClient = new QueryClient();
     const axiosMock = new AxiosMockAdapter(axios);
 
     beforeEach(() => {
+        jest.clearAllMocks();
         axiosMock.reset();
         axiosMock.resetHistory();
         axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither);
@@ -30,10 +40,52 @@ describe("HomePage tests", () => {
             </QueryClientProvider>
         );
 
+        const mainDiv = screen.getByTestId("HomePage-main-div");
+        expect(mainDiv).toBeInTheDocument();
+        expect(mainDiv).toHaveAttribute("style", "background-size: cover; background-image: url(HomePageBackground.jpg);");
+
         const title = screen.getByTestId("homePage-title");
         expect(title).toBeInTheDocument();
         expect(typeof (title.textContent)).toBe('string');
         expect(title.textContent).toEqual('Howdy Farmer');
+    });
+
+    test("renders with default for commons when api times out", () => {
+        axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.userOnly);
+        axiosMock.onGet("/api/commons/all").timeout();
+        render(
+            <QueryClientProvider client={queryClient}>
+                <MemoryRouter>
+                    <HomePage />
+                </MemoryRouter>
+            </QueryClientProvider>
+        );
+
+        const mainDiv = screen.getByTestId("HomePage-main-div");
+        expect(mainDiv).toBeInTheDocument();
+        expect(mainDiv).toHaveAttribute("style", "background-size: cover; background-image: url(HomePageBackground.jpg);");
+
+        const title = screen.getByTestId("homePage-title");
+        expect(title).toBeInTheDocument();
+        expect(typeof (title.textContent)).toBe('string');
+        expect(title.textContent).toEqual('Howdy Farmer');
+
+        expect(() => screen.getAllByTestId(/commonsCard-button/)).toThrow('Unable to find an element');
+    });
+
+    test("expected CSS properties", () => {
+        axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.userOnly);
+        axiosMock.onGet("/api/commons/all").reply(200, []);
+        render(
+            <QueryClientProvider client={queryClient}>
+                <MemoryRouter>
+                    <HomePage />
+                </MemoryRouter>
+            </QueryClientProvider>
+        );
+
+        const title = screen.getByTestId("homePage-title");
+        expect(title).toHaveAttribute("style", "font-size: 75px; border-radius: 7px; background-color: white; opacity: 0.9;");
     });
 
     test("renders without crashing when lists are full", () => {
@@ -69,6 +121,10 @@ describe("HomePage tests", () => {
         expect(await screen.findByTestId("commonsCard-button-Visit-1")).toBeInTheDocument();
         const visitButton = screen.getByTestId("commonsCard-button-Visit-1");
         fireEvent.click(visitButton);
+
+        await waitFor(() => {
+            expect(mockNavigate).toHaveBeenCalledWith("/play/1");
+        });
     });
 
     test("Calls the callback when you click join", async () => {
@@ -77,6 +133,7 @@ describe("HomePage tests", () => {
         axiosMock.onGet("/api/commons/all").reply(200, commonsFixtures.threeCommons);
         axiosMock.onPost("/api/commons/join").reply(200, commonsFixtures.threeCommons[0]);
 
+        
         render(
             <QueryClientProvider client={queryClient}>
                 <MemoryRouter>
@@ -88,5 +145,16 @@ describe("HomePage tests", () => {
         expect(await screen.findByTestId("commonsCard-button-Join-4")).toBeInTheDocument();
         const joinButton = screen.getByTestId("commonsCard-button-Join-4");
         fireEvent.click(joinButton);
+
+        await waitFor(() => {
+            expect(axiosMock.history.post.length).toBe(1);
+            expect(axiosMock.history.post[0].url).toBe("/api/commons/join");
+            expect(axiosMock.history.post[0].params).toEqual({ "commonsId": 4 });
+        });
+
+    
     });
+
+
+
 });
