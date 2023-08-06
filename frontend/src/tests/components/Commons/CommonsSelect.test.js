247982689dedbe8b27b42aaa15079518d49f02b2
Phillip Conrad <pconrad@cs.ucsb.edu>
2023-08-06 16:01:36 -0700

    pc - add frontend for instructor reports jobs

---
+++ frontend/src/tests/components/Commons/CommonsSelect.test.js
@@ -0,0 +1,33 @@
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter as Router } from "react-router-dom";
+import CommonsSelect from "main/components/Commons/CommonsSelect";
+import { QueryClient, QueryClientProvider } from "react-query";
+import commonsFixtures from "fixtures/commonsFixtures"
+
+describe("CommonsSelect tests", () => {
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  const defaultTestId = "CommonsSelect";
+
+  it("renders correctly", async () => {
+    const submitAction = jest.fn();
+
+    const commons=commonsFixtures.threeCommons;
+
+    render(
+      <QueryClientProvider client={new QueryClient()}>
+        <Router>
+          <CommonsSelect commons={commons} handleCommonsSelection={()=>{}} selectedCommons={{}}  />
+        </Router>
+      </QueryClientProvider>
+    );
+
+    expect(await screen.findByTestId(`${defaultTestId}-CommonsSelect-div`)).toBeInTheDocument();
+
+  });
+
+
+});
