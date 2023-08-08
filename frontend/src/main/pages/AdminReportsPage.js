9c0e4fb4b8c3c0c27e29279a295f12e665b55271
Phill Conrad <pconrad@cs.ucsb.edu>
2023-08-08 11:29:26 -0700

    Update frontend/src/main/pages/AdminReportsPage.js

--- frontend/src/main/pages/AdminReportsPage.js
+++ frontend/src/main/pages/AdminReportsPage.js
@@ -5,7 +5,6 @@ import ReportTable  from 'main/components/Reports/ReportTable';
 
 export default function AdminReportsPage()
 {
-  // const { data: currentUser } = useCurrentUser();
 
   // Stryker disable  all 
   const { data: reports, error: _error, status: _status } =
