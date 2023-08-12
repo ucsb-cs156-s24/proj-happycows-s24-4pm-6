ucsb-cs156-s23/proj-happycows-s23-6pm-2

Show last online time for users

src/main/java/edu/ucsb/cs156/happiercows/controllers/UserInfoController.java
@@ -2,12 +2,17 @@ package edu.ucsb.cs156.happiercows.controllers;
 
 import edu.ucsb.cs156.happiercows.entities.User;
 import edu.ucsb.cs156.happiercows.models.CurrentUser;
-import edu.ucsb.cs156.happiercows.services.CurrentUserService;
+import edu.ucsb.cs156.happiercows.repositories.UserRepository;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
+import java.time.Instant;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,12 +20,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/currentUser")
 @RestController
 public class UserInfoController extends ApiController {
+  @Autowired
+  private UserRepository userRepository;
  
   @ApiOperation(value = "Get information about current user")
   @PreAuthorize("hasRole('ROLE_USER')")
   @GetMapping("")
   public CurrentUser getCurrentUser() {
     return super.getCurrentUser();
+  }
+
+  @ApiOperation(value = "Update user's last online time")
+  @PreAuthorize("hasRole('ROLE_USER')")
+  @PostMapping("/last-online")
+  public ResponseEntity<?> updateLastOnline() {
+    User user = super.getCurrentUser().getUser();
+    user.setLastOnline(Instant.now());
+    userRepository.save(user);
+    return ResponseEntity.ok().build();
   }
 }
 

ucsb-cs156-s24/proj-happycows-s24-4pm-6

src/main/java/edu/ucsb/cs156/happiercows/controllers/UserInfoController.java
@@ -2,12 +2,17 @@ package edu.ucsb.cs156.happiercows.controllers;
 
 import edu.ucsb.cs156.happiercows.entities.User;
 import edu.ucsb.cs156.happiercows.models.CurrentUser;
-import edu.ucsb.cs156.happiercows.services.CurrentUserService;
+import edu.ucsb.cs156.happiercows.repositories.UserRepository;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import io.swagger.v3.oas.annotations.Operation;
 
+import java.time.Instant;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,12 +20,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/currentUser")
 @RestController
 public class UserInfoController extends ApiController {
+  @Autowired
+  private UserRepository userRepository;
  
   @Operation(summary = "Get information about current user")
   @PreAuthorize("hasRole('ROLE_USER')")
   @GetMapping("")
   public CurrentUser getCurrentUser() {
     return super.getCurrentUser();
+  }
+
+  @Operation(summary = "Update user's last online time")
+  @PreAuthorize("hasRole('ROLE_USER')")
+  @PostMapping("/last-online")
+  public ResponseEntity<?> updateLastOnline() {
+    User user = super.getCurrentUser().getUser();
+    user.setLastOnline(Instant.now());
+    userRepository.save(user);
+    return ResponseEntity.ok().build();
   }
 }
 