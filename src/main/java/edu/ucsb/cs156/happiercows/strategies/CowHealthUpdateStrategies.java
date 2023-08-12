c3db40134aea1f15da259a4a39bd41e78a60d43d
Matt Priston <mpristoncarruthersfi@ucsb.edu>
2023-08-12 15:27:10 -0700

    <feat>: Cow's health now update using effective capacity

--- src/main/java/edu/ucsb/cs156/happiercows/strategies/CowHealthUpdateStrategies.java
+++ src/main/java/edu/ucsb/cs156/happiercows/strategies/CowHealthUpdateStrategies.java
@@ -19,13 +19,13 @@ public enum CowHealthUpdateStrategies implements CowHealthUpdateStrategy {
     Linear("Linear", "Cow health increases/decreases proportionally to the number of cows over/under the carrying capacity.") {
         @Override
         public double calculateNewCowHealth(Commons commons, UserCommons user, int totalCows) {
-            return user.getCowHealth() - (totalCows - commons.getCarryingCapacity()) * commons.getDegradationRate();
+            return user.getCowHealth() - (totalCows - commons.getEffectiveCapacity()) * commons.getDegradationRate();
         }
     },
     Constant("Constant", "Cow health changes increases/decreases by the degradation rate, depending on if the number of cows exceeds the carrying capacity.") {
         @Override
         public double calculateNewCowHealth(Commons commons, UserCommons user, int totalCows) {
-            if (totalCows <= commons.getCarryingCapacity()) {
+            if (totalCows <= commons.getEffectiveCapacity()) {
                 return user.getCowHealth() + commons.getDegradationRate();
             } else {
                 return user.getCowHealth() - commons.getDegradationRate();
