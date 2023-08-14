d615f43f65008dcfc2cb444b494d8bf8cec23cf8
gento <41165204+dscpsyl@users.noreply.github.com>
2023-08-14 00:05:04 -0700

    feat[playPageGreeting]:Greetings randomly display on play page

--- frontend/src/main/components/Commons/CommonsPlay.js
+++ frontend/src/main/components/Commons/CommonsPlay.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import greetingsList from "../../../assets/PlayGreetings.json"
 
 export default function CommonsPlay({ currentUser }) {
   // Stryker disable next-line OptionalChaining : too many cases to test for too little value
   const firstName = currentUser?.root ? currentUser?.root?.user?.givenName : "";
+
+  const [welcomeText, setWelcomeText]= useState("Welcome farmer");
+
+  useEffect(() => {
+    const numOfGreetings = greetingsList.length;
+    const randomGreeting = greetingsList[Math.floor(Math.random() * numOfGreetings)];
+    setWelcomeText(randomGreeting);
+  }, []);
+
   return (
     <div data-testid="CommonsPlay">
       <h1>
-        Welcome Farmer {firstName}
-      </h1>
+      {welcomeText} {firstName}! 
+    </h1>
     </div>
   );
 };
