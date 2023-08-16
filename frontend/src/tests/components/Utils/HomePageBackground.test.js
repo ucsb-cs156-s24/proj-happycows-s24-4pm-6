5175f7f0bf4aac3e00cabac9cca40ec6102e28a2
gento <41165204+dscpsyl@users.noreply.github.com>
2023-08-16 16:11:54 -0700

    test:Restructure tests to meet mutation guidelines

--- frontend/src/tests/components/Utils/HomePageBackground.test.js
+++ frontend/src/tests/components/Utils/HomePageBackground.test.js
@@ -2,10 +2,35 @@ import getBackgroundImage from "main/components/Utils/HomePageBackground";
 
 
 describe("HomePageBackground tests", () => {
-    test("grabs correct images", () => {
-        expect(getBackgroundImage(7)).toEqual('HomePageBackground-morning.png');
-        expect(getBackgroundImage(12)).toEqual('HomePageBackground-day.png');
-        expect(getBackgroundImage(19)).toEqual('HomePageBackground-evening.png');
-        expect(getBackgroundImage(2)).toEqual('HomePageBackground-night.png');
+    test("expect correct morning background", () => {
+
+        for(let i = 6; i < 9; i++) {
+            expect(getBackgroundImage(i)).toEqual('HomePageBackground-morning.png');
+        }
+    });
+
+    test("expect correct day background", () => {
+
+        for (let i = 9; i < 18; i++) {
+            expect(getBackgroundImage(i)).toEqual('HomePageBackground-day.png');
+        }
+    });
+
+    test("expect correct evening background", () => {
+
+        for (let i = 18; i < 21; i++) {
+            expect(getBackgroundImage(i)).toEqual('HomePageBackground-evening.png');
+        }
+    });
+
+    test("expect correct night background", () => {
+
+        for (let i = 21; i < 24; i++) {
+            expect(getBackgroundImage(i)).toEqual('HomePageBackground-night.png');
+        }
+
+        for (let i = 0; i < 6; i++) {
+            expect(getBackgroundImage(i)).toEqual('HomePageBackground-night.png');
+        }
     });
 });
