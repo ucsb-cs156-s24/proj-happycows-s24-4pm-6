d54586db144718a58e636e6c1ae2ba5bbce723d3
gento <41165204+dscpsyl@users.noreply.github.com>
2023-08-16 21:06:15 -0700

    feat[chat]:Update repository with additional queries

--- src/main/java/edu/ucsb/cs156/happiercows/repositories/ChatMessageRepository.java
+++ src/main/java/edu/ucsb/cs156/happiercows/repositories/ChatMessageRepository.java
@@ -1,11 +1,22 @@
 package edu.ucsb.cs156.happiercows.repositories;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import edu.ucsb.cs156.happiercows.entities.ChatMessage;
 
 @Repository
 public interface ChatMessageRepository extends CrudRepository<ChatMessage, Long>{
-    
+    @Query("SELECT * FROM chat_message cm WHERE cm.commonsId = :commonsId AND cm.hidden = false")
+    Page<ChatMessage> findByCommonsId(Long commonsId, Pageable pageable);
+
+    @Query("SELECT * FROM chat_message cm WHERE cm.commonsId = :commonsId")
+    Iterable<ChatMessage> findAllByCommonsId(Long commonsId);
+
+    @Query("SELECT cm FROM chat_message cm WHERE cm.id = :id")
+    ChatMessage findById(long id);
 }
