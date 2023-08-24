d118306f414624ebad89760f58d2b22e3433ac2c
iain <cakefoxmc@gmail.com>
2023-08-24 13:53:01 -0700

    iw - added templates for Chat components

---
+++ frontend/src/main/components/Chat/ChatMessageDisplay.js
@@ -0,0 +1,13 @@
+import { Card } from 'react-bootstrap';
+
+const ChatMessageDisplay = ({ }) => {
+
+    const testId = "ChatMessageDisplay";
+
+    return (
+        <Card data-testid={testId}>
+        </Card>
+    );
+};
+
+export default ChatMessageDisplay;
