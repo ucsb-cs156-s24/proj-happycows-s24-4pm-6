07f2ee2114c90a5587c79b5df2a7e60bb339ce16
Yashasvi-Vangala <55556296+Yashasvi-Vangala@users.noreply.github.com>
2023-11-17 14:21:07 -0800

    found an issue with test coverage, changed PlayPage.js and now it passes all tests:

--- frontend/src/main/pages/PlayPage.js
+++ frontend/src/main/pages/PlayPage.js
@@ -1,7 +1,7 @@
 import React, {useState} from "react";
 import { Container, CardGroup, Button } from "react-bootstrap";
 import { useParams } from "react-router-dom";
-import { toast } from "react-toastify";
+// import { toast } from "react-toastify";
 
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
 import CommonsOverview from "main/components/Commons/CommonsOverview";
