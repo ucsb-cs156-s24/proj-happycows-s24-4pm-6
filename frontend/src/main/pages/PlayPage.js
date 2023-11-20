9f67792c4ddba72c04b776990b5a14119b7a83fd
Yash Vangala <yvangala@ucsb.edu>
2023-11-20 13:33:59 -0800

    removed commented out package

--- frontend/src/main/pages/PlayPage.js
+++ frontend/src/main/pages/PlayPage.js
@@ -1,7 +1,6 @@
 import React, {useState} from "react";
 import { Container, CardGroup, Button } from "react-bootstrap";
 import { useParams } from "react-router-dom";
-// import { toast } from "react-toastify";
 
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
 import CommonsOverview from "main/components/Commons/CommonsOverview";
