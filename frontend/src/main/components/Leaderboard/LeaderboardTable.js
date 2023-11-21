c9b48d37fff28c6022fb6e00fc75fc57768a7455
haroldmo <72893084+haroldmo@users.noreply.github.com>
2023-11-21 01:26:24 -0800

    Update LeaderboardTable.js (deleted commented code)

--- frontend/src/main/components/Leaderboard/LeaderboardTable.js
+++ frontend/src/main/components/Leaderboard/LeaderboardTable.js
@@ -1,5 +1,4 @@
 import OurTable from "main/components/OurTable";
-// import { hasRole } from "main/utils/currentUser";
 
 // should take in a players list from a commons
 export default function LeaderboardTable({ leaderboardUsers }) {
@@ -10,10 +9,6 @@ export default function LeaderboardTable({ leaderboardUsers }) {
     });
 
     const columns = [
-        // {
-        //     Header: 'User Id',
-        //     accessor: 'userId', 
-        // },
         {
             Header: 'Farmer',
             accessor: 'username'
@@ -72,19 +67,6 @@ export default function LeaderboardTable({ leaderboardUsers }) {
     ];
 
     const testid = "LeaderboardTable";
-
-    /* Temp filler for admin leaderboard table */
-
-    // const columnsIfAdmin = [
-    //     // {
-    //     //     Header: '(Admin) userCommons Id',
-    //     //     accessor: 'id'
-    //     // },
-    //     ...columns
-
-    // ];
-
-    // const columnsToDisplay = hasRole(currentUser, "ROLE_ADMIN") ? columnsIfAdmin : columns;
 
     return <OurTable
         data={leaderboardUsers}
