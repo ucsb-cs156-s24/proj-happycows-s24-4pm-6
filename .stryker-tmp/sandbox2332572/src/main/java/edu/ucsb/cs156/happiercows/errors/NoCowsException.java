4cc2a87ce75da3978e47f95e06be3fb09d2d04da
Angel G <angel@Angels-MacBook-Pro.local>
2023-11-29 19:40:24 -0800

    Removed junk files

--- .stryker-tmp/sandbox2332572/src/main/java/edu/ucsb/cs156/happiercows/errors/NoCowsException.java
+++
@@ -1,7 +0,0 @@
-package edu.ucsb.cs156.happiercows.errors;
-
-public class NoCowsException extends Exception {
-    public NoCowsException(String messageString){
-      super(messageString);
-    }
-  }
--- .stryker-tmp/sandbox2332572/src/main/java/edu/ucsb/cs156/happiercows/errors/NotEnoughMoneyException.java
+++
@@ -1,7 +0,0 @@
-package edu.ucsb.cs156.happiercows.errors;
-
-public class NotEnoughMoneyException extends Exception {
-    public NotEnoughMoneyException(String messageString){
-        super(messageString);
-    }
-}
