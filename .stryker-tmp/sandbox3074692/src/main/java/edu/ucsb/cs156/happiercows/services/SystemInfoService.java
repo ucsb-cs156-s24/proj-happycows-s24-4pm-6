e563df24c27226943968a2ddb0522d6a382e5ef7
rohankumar2025 <rohankumar@ucsb.edu>
2024-03-04 14:40:29 -0800

    removed temp files

--- .stryker-tmp/sandbox3074692/src/main/java/edu/ucsb/cs156/happiercows/services/SystemInfoService.java
+++
@@ -1,7 +0,0 @@
-package edu.ucsb.cs156.happiercows.services;
-
-import edu.ucsb.cs156.happiercows.models.SystemInfo;
-
-public abstract class SystemInfoService {
-  public abstract SystemInfo getSystemInfo();
-}
--- .stryker-tmp/sandbox3074692/src/main/java/edu/ucsb/cs156/happiercows/services/jobs/JobContextConsumer.java
+++
@@ -1,6 +0,0 @@
-package edu.ucsb.cs156.happiercows.services.jobs;
-
-@FunctionalInterface
-public interface JobContextConsumer {
-  void accept(JobContext c) throws Exception;
-}
