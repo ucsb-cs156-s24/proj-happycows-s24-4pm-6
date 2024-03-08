3778e849e3c39f5f716779b4df855ff8f8f95606
Ritu Kirsur <ritukirsur@gmail.com>
2024-03-07 22:08:54 -0800

    linting fixed

--- frontend/src/tests/components/Commons/CommonsCard.test.js
+++ frontend/src/tests/components/Commons/CommonsCard.test.js
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen, findByRole } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import '@testing-library/jest-dom'
 import CommonsCard from "main/components/Commons/CommonsCard";
 import commonsFixtures from "fixtures/commonsFixtures";
