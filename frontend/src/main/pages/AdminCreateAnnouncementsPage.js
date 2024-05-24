27d565985b3715c0972ff171e264441e3f3691ec
Bryce <brycewang@ucsb.edu>
2024-05-23 21:36:31 -0700

    create announcement page

--- frontend/src/main/pages/AdminCreateAnnouncementsPage.js
+++ frontend/src/main/pages/AdminCreateAnnouncementsPage.js
@@ -1,54 +1,14 @@
 import React from "react";
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
 
-import { useParams } from "react-router-dom";
 import AnnouncementForm from "main/components/Announcement/AnnouncementForm";
-import { Navigate } from 'react-router-dom'
-import { toast } from "react-toastify"
-
-import { useBackendMutation } from "main/utils/useBackend";
 
 const AdminCreateAnnouncementsPage = () => {
-
-    let { commonsId } = useParams();
-
-    const objectToAxiosParams = (announcement) => ({        
-        url: "/api/announcements/post",
-        method: "POST",
-        params: {
-            commonsId: announcement.commonsId,
-            startDate: announcement.startDate,
-            endDate: announcement.endDate,
-            announcementText: announcement.announcementText
-        }
-    });
-
-    const onSuccess = (announcement) => {
-        toast(`Announcement successfully created - id: ${announcement.id}`);
-    }
-   
-    const mutation = useBackendMutation(
-        objectToAxiosParams,
-        { onSuccess },
-        // Stryker disable next-line all : hard to set up test for caching
-        ["/api/announcements/all"]
-    );
-
-    const { isSuccess } = mutation
-
-    const onSubmit = async (data) => {
-        console.log("Submitting data:", { ...data, commonsId });
-        mutation.mutate({ ...data, commonsId });
-    }
-
-    if (isSuccess) {
-        return <Navigate to={`/admin/announcements/${commonsId}`} />
-    }
 
     return (
         <BasicLayout>
             <h2>Create Announcement</h2>
-            <AnnouncementForm submitAction={onSubmit}/>
+            <AnnouncementForm />
         </BasicLayout>
     );
 };
---
+++ frontend/src/tests/pages/AdminCreateAnnouncementsPage.test.js
@@ -0,0 +1,58 @@
+import {fireEvent, render, screen, waitFor} from "@testing-library/react";
+import {QueryClient, QueryClientProvider} from "react-query";
+import {MemoryRouter} from "react-router-dom";
+import axios from "axios";
+import AxiosMockAdapter from "axios-mock-adapter";
+
+import {apiCurrentUserFixtures} from "fixtures/currentUserFixtures";
+import {systemInfoFixtures} from "fixtures/systemInfoFixtures";
+import healthUpdateStrategyListFixtures from "../../fixtures/healthUpdateStrategyListFixtures";
+import AdminCreateAnnouncementsPage from "main/pages/AdminCreateAnnouncementsPage";
+
+const mockedNavigate = jest.fn();
+jest.mock('react-router-dom', () => {
+    const originalModule = jest.requireActual('react-router-dom');
+    return {
+        __esModule: true,
+        ...originalModule,
+        Navigate: (x) => { mockedNavigate(x); return null; }
+    };
+});
+
+const mockToast = jest.fn();
+jest.mock('react-toastify', () => {
+    const originalModule = jest.requireActual('react-toastify');
+    return {
+        __esModule: true,
+        ...originalModule,
+        toast: (x) => mockToast(x)
+    };
+});
+
+describe("AdminCreateAnnouncementsPage tests", () => {
+    const axiosMock = new AxiosMockAdapter(axios);
+    const queryClient = new QueryClient();
+
+    beforeEach(() => {
+        axiosMock.reset();
+        axiosMock.resetHistory();
+        axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.userOnly);
+        axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither);
+
+        axiosMock.onGet("/api/commons/all-health-update-strategies")
+            .reply(200, healthUpdateStrategyListFixtures.simple);
+    });
+
+    test("renders without crashing", async () => {
+        render(
+            <QueryClientProvider client={queryClient}>
+                <MemoryRouter>
+                    <AdminCreateAnnouncementsPage />
+                </MemoryRouter>
+            </QueryClientProvider>
+        );
+
+        expect(await screen.findByText("Create Announcement")).toBeInTheDocument();
+    });
+
+});
