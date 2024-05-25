13ebacd1fe506cfb2df1c0d795ada04abed71e45
Phillip Conrad <pconrad@cs.ucsb.edu>
2024-05-24 19:23:19 -0700

    pc - fix mutation coverage issue in CSV helpers
    The previous code used to work but there must have been a change in pitest.

---
+++ src/test/java/edu/ucsb/cs156/happiercows/helpers/ReportCSVHelperTests.java
@@ -0,0 +1,14 @@
+package edu.ucsb.cs156.happiercows.helpers;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+public class ReportCSVHelperTests {
+    
+    @Test
+    void call_constructor_so_pitest_does_not_complain() throws Exception {
+        ReportCSVHelper helper = new ReportCSVHelper();
+        assertTrue(true);
+    }
+}
+
